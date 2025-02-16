b60361b570f3102d3f3f15c8702c5f031a4cbb6d
Jingwu Wang <jingwuwang.2001@gmail.com>
2025-02-16 15:06:08 -0600

    Feature: Game list manager and data manager

--- mini-app/src/DataManagers/GameListManager.ts
+++ mini-app/src/DataManagers/GameListManager.ts
@@ -1,6 +1,42 @@
 import { StubDB } from "../StubData";
+import { GameInfo, User } from "../Types";
 
-// Just checking it's able to access database
-export function logstubGames() {
-    console.log(StubDB.Games)
+function GameListManager() {
+  
+    function getGames(username: string): GameInfo[] {
+        if (!StubDB.Users.find(user => user.name === username)) {
+            throw new Error("User not found");
+        }
+        
+        var userInfo: User = StubDB.Users.find(user => user.name === username)!;
+        var games: GameInfo[] = StubDB.Games;
+        var gamesByUser: GameInfo[] = games.filter(game => userInfo.assignments.includes(game.Name));
+        return gamesByUser;
+    }
+    
+    function getGamesSortedByDeadline(username: string): GameInfo[] {
+        if (!StubDB.Users.find(user => user.name === username)) {
+            throw new Error("User not found");
+        }
+        
+        var userInfo: User = StubDB.Users.find(user => user.name === username)!;
+        var games: GameInfo[] = StubDB.Games;
+        var gamesByUser: GameInfo[] = games.filter(game => userInfo.assignments.includes(game.Name));
+        gamesByUser.sort((a, b) => a.Deadline - b.Deadline);
+        return gamesByUser;
+    }
+
+    function getGameSortedByScore(username: string): GameInfo[] {
+        if (!StubDB.Users.find(user => user.name === username)) {
+            throw new Error("User not found");
+        }
+        
+        var userInfo: User = StubDB.Users.find(user => user.name === username)!;
+        var games: GameInfo[] = StubDB.Games;
+        var gamesByUser: GameInfo[] = games.filter(game => userInfo.assignments.includes(game.Name));
+        gamesByUser.sort((a, b) => userInfo.scores[a.Name] - userInfo.scores[b.Name]);
+        return gamesByUser;
+    }
 }
+
+export default GameListManager;
