e8799cfcfb4a4d6ed5b679800e7d6830175ffb12
Sheena Bai <sheenabai@Sheenas-Air.attlocal.net>
2025-02-19 10:14:33 -0600

    page change to display introduction

--- engine-frontend/src/app/page.tsx
+++ engine-frontend/src/app/page.tsx
@@ -1,6 +1,14 @@
 import { Introduction } from "../Components/introduction";
+// import { SignIn } from "../Components/signIn";
+// import { Home } from "../Components/home";
+// import { GameDescription } from "../Components/gameDescription";
+// import { Lobby } from "../Components/lobby";
+// import { Game } from "../Components/game";
 
 
-export default function App() {
+
+
+export default function Home() {
   return <Introduction />;
 }
+
