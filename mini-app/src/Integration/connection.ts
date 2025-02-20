e5e5721d3fc7d66a3712eeea27d58e5417fdf115
Josue Jacome <josuejacome379@gmail.com>
2025-02-19 21:41:24 -0600

    Sends and receives commands to the WU-A backend (commands are messages)
    Using a new GameManager class, the GameManager abstracts the
    communication to the backend so that the GameInterfacePage doesn't know
    how messages are relayed between each other.
    
    Task #2151 and #2150

--- mini-app/src/Integration/connection.ts
+++ mini-app/src/Integration/connection.ts
@@ -1,6 +1,14 @@
+import { RefObject } from "react";
+import { ApiReceiveFunction, ConnectionManager } from "./ConnectionManager";
+import MessageManager from "./MessageManager";
+
 class Client2ServerAdapter {
+    connectionManager!: ConnectionManager;
+    // roomManager!: RoomManager;
+    messageManager!: MessageManager;
+
     constructor() {}
-  
+
     /**
      * Initializes the adapter by negotiating a connection with the backend.
      * Fetches connection details and sets up managers for rooms and messages.
@@ -31,6 +39,13 @@ class Client2ServerAdapter {
           if (!connectionUrl || !accessToken) {
             throw new Error("Negotiation response is missing required fields.");
           }
+
+          this.connectionManager = new ConnectionManager(
+            connectionUrl,
+            accessToken
+          );
+
+          this.messageManager = new MessageManager(this.connectionManager);
           
         })
         .catch((error) => {
@@ -38,10 +53,72 @@ class Client2ServerAdapter {
           console.error("Negotiation failed:", error);
         });
     }
+
+    // TODO: rename following methods to new API names
+
+    /**
+     * Sends a text message to a chat room.
+     * @param {string} message - The message content.
+     * @param {string} roomId - The ID of the room where the message is sent.
+     * @param {string} username - The sender's username.
+     */ 
+    sendTextMessage(message: string, roomId: string, username: string): void {
+      this.messageManager.sendMessage(message, roomId, username);
+    }
+
+    /**
+     * Joins a chat room.
+     * 
+     * @param {string} roomId - The ID of the room to join.
+     * @param {string} username - The sender's username.
+     */ 
+    joinRoom(roomId: string, username: string): void {
+      return this.messageManager.joinRoom(roomId, username);
+    }
+
+    /**
+     * Sets the message handler callback function. This callback function is invoked whenever
+     * the frontend receives a new message.
+     * 
+     * @param receiveMessageHandler Callback function; handles the new message
+     * @returns 
+     */
+    onReceiveMessage(receiveMessageHandler: (...args: any) => void): ApiReceiveFunction {
+      return this.connectionManager.registerMessageHandler(
+        "NewMessage", // TODO: change name
+        receiveMessageHandler
+      )
+    }
+
+    /**
+     * Removes the provided callback function when registering the onReceiveMessage message
+     * handler.
+     * 
+     * @param receiveMessageHandler The callback function to remove
+     */
+    dismountOnReceiveMessage(receiveMessageHandler: (...args: any) => void): void {
+      // TODO: change NewMessageName
+      this.connectionManager.removeMessageHandler("NewMessage", receiveMessageHandler);
+    }
 }
 
-
-
+/**
+ * Initializes the adapter and notifies the caller that the adapter was initialized (if successful). 
+ * 
+ * @param setIsInitialized Callback function. Function invoked with true if successful, otherwise not invoked.
+ * @param hasInitialized Alternate way to notify caller by setting reference to true if successful.
+ */
+export const initializeAdapter = async (setIsInitialized: (arg0: boolean) => any, hasInitialized: RefObject<Boolean>) => {
+  try {
+    console.log("Initializing client2ServerAdapter...");
+    await client2ServerAdapter.initialize();
+    console.log("client2ServerAdapter initialized successfully!");
+    setIsInitialized(true); // Mark initialization as complete
+    hasInitialized.current = true; // Mark as initialized
+  } catch (error) {
+    console.error("Failed to initialize client2ServerAdapter:", error);
+  }
+};
 
 const client2ServerAdapter = new Client2ServerAdapter();
 export default client2ServerAdapter;
